1505fdab87efd3c717c6b32b97e9ddb266029503
Dario Susnisky <dario.s@techo.org>
2017-09-21 09:15:30 -0300

    Modelos

---
+++ app/Genero.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Genero extends Model
+{
+    public $table = "generos";
+    //public $primaryKey = "id";
+    //public $timestamps = false;
+    public $guarded = [];
+}
---
+++ app/Pelicula.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pelicula extends Model
+{
+    public $table = "movies";
+    //public $primaryKey = "idPelicula";
+    //public $timestamps = false;
+    //Aclararle a Laravel campos modificables
+
+    //Opcion 1: Decirle a Laravel cuales SI puede modificar
+    public $fillable = ["id","title","ranking"];
+}
